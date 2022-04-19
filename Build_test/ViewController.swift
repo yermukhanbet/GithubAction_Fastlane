12cbddc0a7baefb6d13e95eb87d8f41a62d178dc
Yessen <y.yesen.y@gmail.com>
2022-04-19 14:40:31 +0900

    qwe
    qwe

--- Build_test/ViewController.swift
+++ Build_test/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     }
     
     private func forMinorUpdate() {
-        print("testsddsa sdfscxzdf")
+        print("testsddsaasd sdfscxzdf")
     }
 
 }
