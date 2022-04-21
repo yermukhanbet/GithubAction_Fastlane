b5c5b16f54ed6512aacd2405c6ac2ff613751063
Yessen <y.yesen.y@gmail.com>
2022-04-21 15:45:10 +0900

    re

--- Build_test/ViewController.swift
+++ Build_test/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     }
     
     private func forMinorUpdate() {
-        print("testsddsa sdfscxzdsdfdsassdfsadf")
+        print("testsddsa sdfscxzdsdfdsassdsdffsadf")
     }
 
 }
